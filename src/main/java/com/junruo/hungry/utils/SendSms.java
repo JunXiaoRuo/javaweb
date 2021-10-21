9b09ad6b3c64c8619b4c6e6eac27c0baec15eafb
junruo <1813774883@qq.com>
2021-10-21 12:56:10 +0800

    第一次提交

--- src/main/java/com/junruo/hungry/utils/SendSms.java
+++ src/main/java/com/junruo/hungry/utils/SendSms.java
@@ -47,7 +47,7 @@ public class SendSms
         try{
             // 实例化一个认证对象，入参需要传入腾讯云账户secretId，secretKey,此处还需注意密钥对的保密
             // 密钥可前往https://console.cloud.tencent.com/cam/capi网站进行获取
-            Credential cred = new Credential("AKID3wZw2rRknZ6ML3UfZCZgW5NO8bniNls2", "vZw2rQiiY8RTEjxtYz94h38wTjiRs2vX");
+            Credential cred = new Credential("AKIDg9MIyNWP1MQEl11nJZZKo1xc190atvgA", "EBrHRKxKQGp78sLXT8XgY9UYLr77qITM");
             // 实例化一个http选项，可选的，没有特殊需求可以跳过
             HttpProfile httpProfile = new HttpProfile();
             httpProfile.setEndpoint("sms.tencentcloudapi.com");
